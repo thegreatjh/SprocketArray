109a47db7517d0c262237ba1a188b5a7d9529a3f
thegreatjh <74930658+thegreatjh@users.noreply.github.com>
2020-11-28 00:19:36 -0700

    Update SprocketsArrayJasonHasleton.java

--- SprocketsArrayJasonHasleton.java
+++ SprocketsArrayJasonHasleton.java
@@ -26,60 +26,15 @@ public class SprocketsArrayJasonHasleton
         jf.add(jsp);
         jf.setVisible(true);
 
-        int c=0;
+        int c = 0;
         
-        int[] order;
-        order = new int[51];
-        order[1] = 0;
-        order[2] = 0;
-        order[3] = 0;
-        order[4] = 0;
-        order[5] = 0;
-        order[6] = 0;
-        order[7] = 0;
-        order[8] = 0;
-        order[9] = 0;
-        order[10] = 0;
-        order[11] = 0;
-        order[12] = 0;
-        order[13] = 0;
-        order[14] = 0;
-        order[15] = 0;
-        order[16] = 0;
-        order[17] = 0;
-        order[18] = 0;
-        order[19] = 0;
-        order[20] = 0;
-        order[21] = 0;
-        order[22] = 0;
-        order[23] = 0;
-        order[24] = 0;
-        order[25] = 0;
-        order[26] = 0;
-        order[27] = 0;
-        order[28] = 0;
-        order[29] = 0;
-        order[30] = 0;
-        order[31] = 0;
-        order[32] = 0;
-        order[33] = 0;
-        order[34] = 0;
-        order[35] = 0;
-        order[36] = 0;
-        order[37] = 0;
-        order[38] = 0;
-        order[39] = 0;
-        order[40] = 0;
-        order[41] = 0;
-        order[42] = 0;
-        order[43] = 0;
-        order[44] = 0;
-        order[45] = 0;
-        order[46] = 0;
-        order[47] = 0;
-        order[48] = 0;
-        order[49] = 0;
-        order[50] = 0;
+        while (c < 50)
+        {
+            c++;
+            order[c] = 0;
+        }
+
+        c = 0;
               
         while (!report.eof())
         {
